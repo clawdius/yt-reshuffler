7278717c78580f90a30a203900c34debd61693f3
Aditya Lila <aditapik@gmail.com>
2025-05-05 10:52:41 +0700

    chore: RPC workaround for deleted video missing id

--- main/utils/richPresenceUtils.js
+++ main/utils/richPresenceUtils.js
@@ -1,13 +1,16 @@
+const startTime = new Date();
+
 function rpcSetActivity(handlersData, rpcPayload) {
     const { title, details, playlistName = "", id = "" } = rpcPayload;
     handlersData.rpc.user.setActivity({
         type: 2,
         details: title,
         state: details,
+        startTimestamp: startTime,
         smallImageKey: id != "" ? "" : "github-icon",
         smallImageText: "YT-Reshuffler on Github",
         largeImageText: playlistName != "" ? playlistName : "",
-        largeImageKey: id != "" ? `https://i.ytimg.com/vi/${id}/maxresdefault.jpg` : ""
+        largeImageKey: id != "" && title != "Deleted Video" ? `https://i.ytimg.com/vi/${id}/maxresdefault.jpg` : ""
     });
 }
 
